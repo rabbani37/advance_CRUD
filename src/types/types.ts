970b281a1105889f627c216de604f4bea9826f24
georabbani <231213066+rabbani37@users.noreply.github.com>
2026-05-19 21:43:43 +0600

    types declear

---
+++ src/types/types.ts
@@ -0,0 +1,27 @@
+export const role = ["user", "admin", "superAdmin"] as const;
+
+type TRole = typeof role[number];
+
+
+type TUser = {
+    id: number;
+    name: string;
+    email: string;
+    password_hash: string;
+    age: number;
+    role: TRole;
+    created_at: Date;
+    update_at: Date;
+}
+
+export type RUser = Omit<TUser, "id" | "password_hash" | "created_at" | "update_at">;
+
+type TOrder = {
+    id: number;
+    customer_id: number;
+    quentity: number;
+    food: string;
+    price:number;
+    created_at: Date;
+    update_at: Date;
+}
