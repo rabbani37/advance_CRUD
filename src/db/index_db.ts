2e9ee083a635447b0762926608172249bbc0813f
georabbani <231213066+rabbani37@users.noreply.github.com>
2026-05-19 18:34:51 +0600

    create roders table into neondb

--- src/db/index_db.ts
+++ src/db/index_db.ts
@@ -5,17 +5,32 @@ const sql = neon(config.database_usrl);
 
 export const initDB = async () => {
 
-    await sql`
+    // create users table
+    await sql`  
     CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     name VARCHAR(75) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     age INT,
-    role VARCHAR(20) NOT NULL DEFAULT user,
+    role VARCHAR(20) NOT NULL DEFAULT 'user',
 
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     update_at TIMESTAMP NOT NULL DEFAULT NOW()
-
     )`;
+
+    // create order tables 
+    await sql`
+    CREATE TABLE IF NOT EXISTS orders(
+    id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    quentity INT NOT NULL,
+    food TEXT NOT NULL,
+    price NUMERIC(10, 2) NOT NULL,
+
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    update_at TIMESTAMP NOT NULL DEFAULT NOW()
+    )`;
+
+
     console.log('Database connected successfully...');
 }
